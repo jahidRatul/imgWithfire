acbfdfcf3280e18fa0fa90b1a631062e572f9be1
jahidRatul <jahidratul@gmail.com>
2020-02-08 02:54:01 +0600

    uploaded img

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'img with fire',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File sampleImage;
+
+  Future getImage() async {
+    var tempImage = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      sampleImage = tempImage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Upload'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: sampleImage == null ? Text('Select an image') : enableUpload(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Add Image',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget enableUpload() {
+    int dateTime = DateTime.now().millisecondsSinceEpoch;
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Image.file(sampleImage, height: 300.0, width: 300.0),
+          RaisedButton(
+            elevation: 7.0,
+            child: Text('Upload'),
+            textColor: Colors.white,
+            color: Colors.blue,
+            onPressed: () async {
+              final StorageReference storageRef =
+                  FirebaseStorage.instance.ref().child(dateTime.toString());
+              final StorageUploadTask task = storageRef.putFile(sampleImage);
+              final StorageTaskSnapshot imgUrl = (await task.onComplete);
+              final String url = (await imgUrl.ref.getDownloadURL());
+              print('Url is $url');
+//              String imageUrl = await storageRef.getDownloadURL();
+//              print('2nd $imageUrl');
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
